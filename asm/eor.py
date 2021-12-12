06b6d0f593885e2100df1fb1aa5d488c0a79f3a7
Username <user@example.com>
2021-12-12 12:36:54 +0100

    Pythonization

--- asm/eor.py
+++ asm/eor.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python3
 import sys
 
 eor = 0
-
 with open(sys.argv[1], "rb") as f:
     while True:
         c = f.read(1)
-        if len(c) == 0:
+        if not c:
             break
-        eor = eor ^ ord(c)
+        eor ^= ord(c)
 
 print(hex(eor))
