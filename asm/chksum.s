1ea8270a322f991148bab1fb13702d127f875849
Username <user@example.com>
2021-12-12 12:25:33 +0100

    Fix typo in example

--- asm/chksum.s
+++ asm/chksum.s
@@ -1,26 +1,25 @@
 ; Will do an EOR on a memory range
 ; Usage: to do between 2A0 and 2DE
-; FA: 0A 02 DE 02
+; FA: A0 02 DE 02
 ; 
 
-STARTL      equ $FA
-STARTH      equ $FB
-ENDL        equ $FC
-ENDH        equ $FD
-CURL        equ $FE
-CURH        equ $FF
-RES         equ $EC
-COUT1       equ $FDF0
+STARTL      equ     $FA
+STARTH      equ     $FB
+ENDL        equ     $FC
+ENDH        equ     $FD
+CURL        equ     $FE
+CURH        equ     $FF
+RES         equ     $EC
+COUT1       equ     $FDF0
 
-            org $2A0
+            org     $2A0
 INIT
-            lda #0
-            sta CURL
-            ldy STARTL
-            ldx STARTH
-            stx CURH
-            sta RES
-
+            lda     #0
+            sta     CURL
+            sta     RES
+            ldx     STARTH
+            stx     CURH
+            ldy     STARTL
 LOOP1
             ldx     CURH
             cpx     ENDH
@@ -32,7 +31,6 @@ LOOP1
             bne     LOOP1       ;
             inc     CURH        ; next page
             jmp     LOOP1
-
 LASTLOOP    
             eor     (CURL),y
 
@@ -43,7 +41,6 @@ FINISH
             sta     RES
             jsr     COUTBYTE
             rts
-
 COUT4
             and     #$0F
             ora     #$B0        ; convert to ASCII for number
@@ -53,7 +50,6 @@ COUT4
 COUTN
             jsr     COUT1
             rts
-
 COUTBYTE
             pha
             pha
